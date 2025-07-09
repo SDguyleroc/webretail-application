faa5d633fcf45656d62c3870f2ee8df711543019
ossebigc <guy-leroc.ossebi@nssnet.com>
2025-07-09 13:52:54 -0400

    added JPA and h2 Database Dependency and updated the User class

--- src/main/java/com/app_ossebi/webretail/User.java
+++ src/main/java/com/app_ossebi/webretail/User.java
@@ -1,16 +1,28 @@
 package com.app_ossebi.webretail;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * Represents a user in the system.
  * This class is a data model containing user information.
  */
-@Data
+@Data // Generates getter and setter methods
+// @Entity(name = "user_table") // Indicates that this class is a JPA entity and should be mapped to a database table
+@NoArgsConstructor // Generates a no-argument constructor
+//@AllArgsConstructor // Generates a constructor with all arguments
+@Entity
 public class User {
     /**
      * The unique identifier for the user.
      */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     
     /**
---
+++ src/main/java/com/app_ossebi/webretail/UserRepository.java
@@ -0,0 +1,4 @@
+package com.app_ossebi.webretail;
+
+public interface UserRepository {
+}
